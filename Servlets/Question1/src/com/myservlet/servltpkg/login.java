97420092f9ba9515dafe9c14154f9baa0ff1fae7
ChauhanShruti <shruti.chauhan@newgen.co.in>
2016-07-07 09:44:59 +0530

    Add files via upload

---
+++ Servlets/Question1/src/com/myservlet/servltpkg/login.java
@@ -0,0 +1,70 @@
+package com.myservlet.servltpkg;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String email=request.getParameter("email");
+		String password=request.getParameter("password");
+		
+		if("Kriti@gmail.com".equalsIgnoreCase(email) && "pass".equalsIgnoreCase(password)){
+			RequestDispatcher rd = request.getRequestDispatcher("welcome");
+			rd.forward(request,response);
+		}else{
+			RequestDispatcher rd = request.getRequestDispatcher("/index.html");
+			rd.include(request, response);
+			out.println("<i> Error : Enter valid email address & password </i> " );
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
